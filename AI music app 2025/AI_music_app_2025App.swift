9c1b2be2405b6e2f809b03eb0ef70eb7c3b630c4
raphaelle <raphaellegh@gmail.com>
2025-04-20 09:32:00 -0400

    Initial Commit

---
+++ AI music app 2025/AI_music_app_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AI_music_app_2025App.swift
+//  AI music app 2025
+//
+//  Created by Raph Ghanem on 2025-04-20.
+//
+
+import SwiftUI
+
+@main
+struct AI_music_app_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
